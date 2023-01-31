cad0c1ad16ecebb10ba53ef87042539adac5f9a3
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-01-31 15:43:58 +0100

    mise en pratique useRouter

--- blog-nextjs/pages/blog/[slug].js
+++ blog-nextjs/pages/blog/[slug].js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
 
 export default function article() {
+  const router = useRouter();
+
+  console.log(router);
+  console.log(router.query);
+  console.log(router.query.slug);
+  console.log(router.asPath);
+//méthode push pour ramplacer le link de next
+  const pushFonction = () => {
+    router.push("/");
+  };
+
   return (
-    <div>10 plats savoureux</div>
-  )
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={pushFonction}> home</button>
+    </div>
+  );
 }
