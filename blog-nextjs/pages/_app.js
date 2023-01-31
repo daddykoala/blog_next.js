4d9aa68239433c108b1d88a4eba817d0920ce273
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-01-31 16:01:39 +0100

    mep: element englobant APP

--- blog-nextjs/pages/_app.js
+++ blog-nextjs/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import Container from '@/component/container/Container'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+
+      <Component {...pageProps} />
+    </Container>
+    
+  )
 }
