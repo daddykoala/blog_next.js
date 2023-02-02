425d80e1359e2830a4971697ec3806375363e176
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-02-02 15:23:46 +0100

    Mep de revalidate

--- blog-nextjs/pages/contact.js
+++ blog-nextjs/pages/contact.js
@@ -1,7 +1,25 @@
+import React from "react"
 
-
-export default function contact() {
-  return (
-    <div>contact</div>
-  )
+export default function contact(props) {
+  console.log(props);
+  return <div>contact</div>;
 }
+
+export async function getStaticProps() {
+  console.log("OLA1")
+  const quote = await fetch(
+    "https://jsonplaceholder.typicode.com/posts/1/comments"
+  );
+  console.log(quote,"OLA2");
+  const data = await quote.json()
+
+  return {
+    props: {
+      data
+    },
+    // revalidate: 10 va permettre de recharger la page toutes les 10 secondes
+    revalidate: 10
+  };
+}
+
+//https://goquotes-api.herokuapp.com/api/v1/random?count=1
