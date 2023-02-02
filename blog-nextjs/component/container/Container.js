75453ed4a3765d4009d166661565979b9290868d
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-02-02 13:27:12 +0100

    creation d'un container gloal

--- blog-nextjs/component/container/Container.js
+++ blog-nextjs/component/container/Container.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 
+
+//ici nous avons un composant qui va englober tout les autres composants
+//il va nous permettre de ne pas avoir a répéter le code de la navbar
+//dans chaque composant
+//on appelle ca un wrapper
 export default function Container(props) {
   return (
     <div>
--- blog-nextjs/pages/_app.js
+++ blog-nextjs/pages/_app.js
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import Container from '@/component/container/Container'
 
+
+//j'imlport le composant container et je l'englobe autour de tout les autres composants(un peu comme un provider)
+
+//les pagesProps sont les props qui sont passées par nextjs.
 export default function App({ Component, pageProps }) {
   return (
     <Container>
