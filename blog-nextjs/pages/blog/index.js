0ac5f21f7873e99ccb74873e8bb71e09555b2247
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-02-02 14:37:54 +0100

    mise en place mathode getStaticProps

--- blog-nextjs/pages/blog/index.js
+++ blog-nextjs/pages/blog/index.js
@@ -1,5 +1,11 @@
 
-
+//diference entre une app react et une app nextjs
+//nextjs est une app react avec des fonctionnalités en plus
+//nextjs va creer des pages statiques pour chaque page de notre app.
+//difference entre le rendu statique et le rendu serveur
+//le rendu statique est le rendu qui se fait coté client
+//
+//le rendu serveur est le rendu qui se fait coté serveur
 export default function index() {
   return (
     <div>le blog
--- blog-nextjs/pages/index.js
+++ blog-nextjs/pages/index.js
@@ -6,17 +6,53 @@ import Navbar from '@/component/Navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+
+//pour utiliser les données locales il faut utiliser la function getStaticProps
+// et faire passer les données en props
+export default function Home(props) {
+
+  console.log(props);
 
   const id = "article"
   return (
     <>
     
     <div className={styles.container}>
-    <h1> hello </h1>
+    <h1> Vocabulaire de base</h1>
+    <table className={styles.tableau}>
+      <tbody>
+        {/* si dans la methode maps on a un seul parametre on peut l'ecriture sans parenthese
+        si l'on met des parenthes et des crochets on doit !!! mettre un return !!! 
+        les parentheses induisent le return pas les crochets*/}
+        {props.array.map((element) => {
+          return(
+
+          <tr>
+            <td>{element.en}</td>
+            <td>{element.fr}</td>
+          </tr>
+          )
+        })}
+      </tbody>
+    </table>
     
     </div>
     </>
     
   )
+}
+
+//getStaticProps va s'executer seulement losqu'on va build notre application l'on pourrait mettre des credentials ici ou des clefs d'api etc... 
+//la function get static props est une function qui va nous permettre de récupérer des données avant que la page soit rendue.
+export async function getStaticProps() {
+  //j'utilise la methode import pour importer les données locales.
+ const data = await import ('../data/vocabulary.json')
+ //je les assigne dans un tableau
+ const array = data.vocabulary;
+//tres important je retounre un objet avec un props qui contient le tableau
+ return{
+  props:{
+    array
+  }
+ }
 }
