0678c1e370283a7a7bc70532cd686af9ee969108
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-02-10 15:15:17 +0100

    initialisation projet blog

--- blog-nextjs/pages/[profil]/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-//toujours mettre le nom du fichier en minuscule
-//et toujours mettre le nom du composant en majuscule
-export default function Index() {
-  return (
-    <div>votre profil</div>
-  )
-}
